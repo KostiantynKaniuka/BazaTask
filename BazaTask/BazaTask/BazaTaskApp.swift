6fedc5a6ce5b04b43956d78e0bfbec0b180e5875
Kostiantyn Kaniuka <heyhocombo@gmail.com>
2025-09-01 19:53:07 +0200

    adding git ignore

---
+++ BazaTask/BazaTask/BazaTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BazaTaskApp.swift
+//  BazaTask
+//
+//  Created by Kostiantyn Kaniuka on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BazaTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
